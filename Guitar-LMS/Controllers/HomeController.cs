b3a51be0e2f9a7527b9870cc424499f3bd134c56
Rudy.M <Rudy.M@fanruan.com>
2021-12-16 14:49:00 +0800

    get calendar in

---
+++ Guitar-LMS/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guitar_LMS.Controllers {
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller{
+        public HomeController() {
+        }
+
+        [Route("~/")]
+        [Route("")]
+        public IActionResult Index() {
+            return View();
+        }
+
+        [Route("{dateStr}")]
+        public IActionResult ToDoList(string dateStr) {
+            return View();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+# ASP.NET Core - Guitar Learning Management System
+
+## Motivation
+
+## Introduction
+
+## Features
+
+- [ ] Calendar shows completion status for each day -> Like leetcode attendance  
+- [ ] To-Do list based on date
+- [ ] A summary text section for each day
+- [ ] A overall metric 
+
+## Design
